c9dca0a1f001018e366bfa5186c6b2e8d54dcdd1
Óscar Cordobés Navarro <alu0101478081@ull.edu.es>
2024-03-01 23:10:26 +0000

    capacidad para generar PDF's y HTML's reales

--- src/Ejercicio-2/ExportHTML.ts
+++ src/Ejercicio-2/ExportHTML.ts
@@ -10,30 +10,94 @@
  * Práctica 6: Clases e interfaces genéricas. Principios SOLID
  */
 
-import { Bill } from './Bill';
-import { ExportBill } from './ExportBill';
+import { Bill } from "./Bill";
+import { ExportBill } from "./ExportBill";
+import * as fs from "fs";
 
 export class ExportHTML implements ExportBill {
-    public extention: string = 'html';
+  public extention: string = "html";
 
-    exportBill(bill: Bill): void {
-        console.log(`Exporting bill ${bill.getId()} to HTML`);
-        console.log(this.createHTML(bill));
-    }
+  exportBill(bill: Bill): void {
+    console.log(`Exporting bill ${bill.getId()} to HTML`);
+    console.log(this.createHTML(bill));
+  }
 
-    private createHTML(bill: Bill): string {
-        return `
-            <html>
-                <head>
-                    <title>Bill ${bill.getId()}</title>
-                </head>
-                <body>
-                    <h1>Bill ${bill.getId()}</h1>
-                    <p>Amount: ${bill.getAmount()}€</p>
-                    <p>Date: ${bill.getDate()}</p>
-                    <p>Client: ${bill.getClient()}</p>
-                </body>
-            </html>
-        `;
-    }
+  private createHTML(bill: Bill): string {
+    return this.generateHTML(bill);
+  }
+
+  private generateHTML(bill: Bill) {
+    const htmlString = ` <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bill ${bill.getId()}</title>
+        <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 20px;
+            box-sizing: border-box;
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+            border-bottom: 2px solid #333;
+            padding-bottom: 10px;
+        }
+
+        p {
+            text-align: left;
+            margin-bottom: 8px;
+        }
+
+        .bill-info {
+            background-color: #fff;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            padding: 20px;
+            margin-top: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .amount {
+            font-size: 24px;
+            font-weight: bold;
+            color: #27ae60;
+        }
+
+        .date {
+            color: #333;
+        }
+
+        .client {
+            color: #3498db;
+        }
+
+        .paid-status {
+            color: #e74c3c;
+            font-weight: bold;
+        }
+    </style>
+    </head>
+    
+    <body>
+        <h1>Bill ${bill.getId()}</h1>
+        <p>Amount: ${bill.getAmount()}€</p>
+        <p>Date: ${bill.getDate()}</p>
+        <p>Client: ${bill.getClient()}</p>
+        <p>Paid: ${bill.getPaid() ? "Yes" : "No"}</p>
+    </body>
+    
+    </html>`;
+
+    // Creamos el archivo HTML
+    fs.writeFileSync(`./exports/bill${bill.getId()}.html`, htmlString);
+
+    return "Archivo HTML creado correctamente"
+  }
 }
--- src/Ejercicio-2/ExportPDF.ts
+++ src/Ejercicio-2/ExportPDF.ts
@@ -10,26 +10,69 @@
  * Práctica 6: Clases e interfaces genéricas. Principios SOLID
  */
 
-import { Bill } from './Bill';
-import { ExportBill } from './ExportBill';
+// './exports/bill.pdf'
+
+import { Bill } from "./Bill";
+import { ExportBill } from "./ExportBill";
+import * as PDFDocument from "pdfkit";
+import * as fs from "fs";
 
 export class ExportPDF implements ExportBill {
-    public extention: string = 'pdf';
+  public extention: string = "pdf";
 
-    exportBill(bill: Bill): void {
-        console.log(`Exporting bill ${bill.getId()} to PDF`);
-        console.log(this.createPDF(bill));
+  exportBill(bill: Bill): void {
+    console.log(`Exporting bill ${bill.getId()} to PDF`);
+    console.log(this.createPDF(bill));
+  }
+
+  private createPDF(bill: Bill): string {
+    return this.generatePDF(bill);
+  }
+
+  private generatePDF(bill: Bill) {
+    const doc = new PDFDocument({ size: "A4" });
+    const output = fs.createWriteStream("./exports/bill.pdf");
+
+    if (!fs.existsSync("./exports")) {
+      fs.mkdirSync("./exports");
     }
 
-    private createPDF(bill: Bill): string { // This method is not implemented
-        return `
-        -------------------------- FACTURA --------------------------
-        Numero de factura: ${bill.getId()}
-        Total: ${bill.getAmount()}€
-        Fecha: ${bill.getDate()}
-        A nombre de ${bill.getClient()}
-        ¿Está pagada? ${bill.getPaid() ? 'Sí' : 'No'}
+    doc.fontSize(24);
+    doc.font("Courier-Bold");
+    doc.text("OscarCN INC.", 50, 50, { align: "center" });
+    doc.fontSize(12);
+    doc.moveTo(50, 70).lineTo(550, 70).stroke();
+    doc.moveDown(3);
 
-        `
+    doc.font("Courier-Bold");
+    doc.text(`Numero de factura: ${bill.getId()}`, { align: "center" });
+    doc.moveDown();
+
+    doc.rect(50, 100, 500, 100).stroke();
+    doc.font("Courier");
+    doc.text(`Total: ${bill.getAmount()}€`, { align: "left" });
+    doc.text(`Fecha: ${bill.getDate()}`, { align: "left" });
+    doc.text(`Cliente: ${bill.getClient()}`, { align: "left" });
+    doc.text(`Pagado: ${bill.getPaid() ? "Sí" : "No"}`, { align: "left" });
+
+    //Añadir al final de la página un texto en el centro
+    doc.font("Courier-Bold");
+
+    while (doc.y < 750) {
+      // Si llegamos al final de la página, añadimos un texto
+      if (doc.y > 740) {
+        doc.text(
+          "Este PDF ha sido generado automáticamente utilizando pdfkit",
+          { align: "center" },
+        );
+        break;
+      }
+      doc.moveDown();
     }
+
+    doc.pipe(output);
+    doc.end();
+
+    return `PDF generado en ${output.path}`;
+  }
 }
